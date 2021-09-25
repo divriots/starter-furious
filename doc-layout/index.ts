d03f93c4ddc2bc1e7b2e047531a9c52adcbbb9fb
Gregory Quinio <quinio.gregory@gmail.com>
2021-09-25 20:09:05 +0000

    Fix vite

--- doc-layout/index.ts
+++ doc-layout/index.ts
@@ -1,6 +1,3 @@
-import 'https://cdn.jsdelivr.net/npm/modern-normalize@1.1.0/modern-normalize.min.css';
-import 'https://cdn.jsdelivr.net/npm/@tailwindcss/typography@0.4.1/dist/typography.min.css';
-import 'https://gitcdn.link/repo/PrismJS/prism-themes/master/themes/prism-vsc-dark-plus.css';
 import './styles.css';
 
 document.body.classList.add('prose');
