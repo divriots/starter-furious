76e69a7aaa09192214a8fc89c50351c34e528c46
Mikhail Bashkirov <bashmish@gmail.com>
2021-10-26 12:46:10 +0200

    fix(tokens): make tokens work in non Chromium based browsers

--- core/src/provide-design-system.ts
+++ core/src/provide-design-system.ts
@@ -3,7 +3,7 @@ import { provideTokens } from './provide-tokens';
 
 export const provideDesignSystem = (childElement?: HTMLElement): Pick<DesignSystem, 'register'> => {
   provideTokens(childElement);
-  const ds = DesignSystem.getOrCreate(childElement).withPrefix('fs');
+  const ds = DesignSystem.getOrCreate(childElement).withPrefix('fs').withDesignTokenRoot(document.documentElement);
   return new Proxy(ds, {
     get: (target, prop, ...rest) => {
       if (['withPrefix', 'withShadowRootMode', 'withElementDisambiguation'].includes(prop as string)) {
