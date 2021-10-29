c91592f385aee661d5c423c2f9245d46d1c8591b
Gregory Quinio <1063530+undefined@users.noreply.github.com>
2021-10-29 23:49:30 +0000

    Push from fix-cdn

--- mdjs-layout/index.ts
+++ mdjs-layout/index.ts
@@ -1,5 +1,5 @@
 import 'https://cdn.jsdelivr.net/npm/modern-normalize@1.1.0/modern-normalize.min.css';
-import 'https://gitcdn.link/repo/PrismJS/prism-themes/master/themes/prism-vsc-dark-plus.css';
+import 'https://cdn.jsdelivr.net/npm/prism-themes@1.9.0/themes/prism-vsc-dark-plus.min.css';
 import './typography.css';
 import './misc.css';
 import { MdjsLayout } from './src/mdjs-layout';
