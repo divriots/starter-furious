5ace15efab1f0ec681bcbc647b7aac2bbb977f32
Mikhail Bashkirov <bashmish@gmail.com>
2021-12-09 13:01:13 +0100

    feat(doc-layout): migrate to new mdjs-layout from @divriots/dockit-core

--- doc-layout/src/template.js
+++ doc-layout/src/template.js
@@ -1,14 +1,15 @@
+import '@divriots/dockit-core/mdjs-layout/define';
+import { styles } from '@divriots/dockit-core/mdjs-layout';
 import { baseLayerLuminance, fillColor, provideDesignSystem } from '@divriots/starter-furious';
 import { StandardLuminance } from '@microsoft/fast-components';
 import { html } from '@microsoft/fast-element';
-import '~/mdjs-layout';
 import { logoSvg } from './logo.svg';
 
 provideDesignSystem().register();
 
 export const docLayoutTemplate = (content, context) => html`
   <style>
-    .logo {
+    ${styles} .logo {
       color: #d13400;
     }
     .preview-story .story_padded {
@@ -19,13 +20,15 @@ export const docLayoutTemplate = (content, context) => html`
     :context="${() => context}"
     @color-scheme-change="${(_, c) => {
       if (c.event.detail.colorScheme === 'dark') {
+        document.documentElement.classList.add('dark');
         baseLayerLuminance.setValueFor(document.body, StandardLuminance.DarkMode);
       } else {
+        document.documentElement.classList.remove('dark');
         baseLayerLuminance.setValueFor(document.body, StandardLuminance.LightMode);
       }
     }}"
   >
     <div class="logo" slot="logo" aria-label="starter-furious">${logoSvg}</div>
-    <div class="prose dark:prose-light" :innerHTML="${() => content}"></div>
+    <div class="prose dark:prose-dark" :innerHTML="${() => content}"></div>
   </mdjs-layout>
 `;
