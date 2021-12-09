9fd1da9d3c3e7971a0b0bad1d7fbf7f4a633270f
Mikhail Bashkirov <bashmish@gmail.com>
2021-12-09 13:01:13 +0100

    feat(doc-layout): migrate to new mdjs-layout from @divriots/dockit-core

--- mdjs-layout/index.ts
+++
@@ -1,7 +0,0 @@
-import 'https://cdn.jsdelivr.net/npm/modern-normalize@1.1.0/modern-normalize.min.css';
-import 'https://cdn.jsdelivr.net/npm/prism-themes@1.9.0/themes/prism-vsc-dark-plus.min.css';
-import './typography.css';
-import './misc.css';
-import { MdjsLayout } from './src/mdjs-layout';
-
-customElements.define('mdjs-layout', MdjsLayout);
