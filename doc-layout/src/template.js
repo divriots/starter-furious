15b26d7e3497ccd36ca75daf70ee52b7206ff9d7
Mikhail Bashkirov <bashmish@gmail.com>
2022-03-28 12:46:25 +0200

    chore: use curly braces syntax for the function

--- doc-layout/src/template.js
+++ doc-layout/src/template.js
@@ -7,28 +7,30 @@ import { logoSvg } from './logo.svg';
 
 provideDesignSystem().register();
 
-export const docLayoutTemplate = (content, context) => html`
-  <style>
-    ${styles} .logo {
-      color: #d13400;
-    }
-    .preview-story .story_padded {
-      background-color: ${fillColor.createCSS()};
-    }
-  </style>
-  <dockit-layout
-    :context="${() => context}"
-    @color-scheme-change="${(_, c) => {
-      if (c.event.detail.colorScheme === 'dark') {
-        document.documentElement.classList.add('dark');
-        baseLayerLuminance.setValueFor(document.body, StandardLuminance.DarkMode);
-      } else {
-        document.documentElement.classList.remove('dark');
-        baseLayerLuminance.setValueFor(document.body, StandardLuminance.LightMode);
+export const docLayoutTemplate = (content, context) => {
+  return html`
+    <style>
+      ${styles} .logo {
+        color: #d13400;
       }
-    }}"
-  >
-    <div class="logo" slot="logo" aria-label="starter-furious">${logoSvg}</div>
-    <div class="prose dark:prose-invert" :innerHTML="${() => content}"></div>
-  </dockit-layout>
-`;
+      .preview-story .story_padded {
+        background-color: ${fillColor.createCSS()};
+      }
+    </style>
+    <dockit-layout
+      :context="${() => context}"
+      @color-scheme-change="${(_, c) => {
+        if (c.event.detail.colorScheme === 'dark') {
+          document.documentElement.classList.add('dark');
+          baseLayerLuminance.setValueFor(document.body, StandardLuminance.DarkMode);
+        } else {
+          document.documentElement.classList.remove('dark');
+          baseLayerLuminance.setValueFor(document.body, StandardLuminance.LightMode);
+        }
+      }}"
+    >
+      <div class="logo" slot="logo" aria-label="starter-furious">${logoSvg}</div>
+      <div class="prose dark:prose-invert" :innerHTML="${() => content}"></div>
+    </dockit-layout>
+  `;
+};
